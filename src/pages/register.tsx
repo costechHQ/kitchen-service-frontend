b4826c9ac4a114e7655dbed23853f0680fa04e22
Onyedika Christopher <cosimon.tech@gmail.com>
2026-09-13 14:14:43 +0100

    Feat: commiting

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,3 +1,59 @@
+import { useState } from "react";
+
 export default function Register() {
-  return <div>Register page</div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [address, setAddress] = useState("");
+  const [password, setPassword] = useState("");
+
+  
+    const handleSubmit = () => {
+      console.log({
+        name,
+        email,
+        address,
+        password,
+      });
+    };
+
+  return (
+    <div>
+      <h1>Create an account </h1>
+
+      <input 
+      type="text"
+      placeholder="Name"
+      value={name}
+      onChange={(event) => setName(event.target.value)}
+      />
+
+      <input 
+      type="email"
+      placeholder="Email"
+      value={email}
+      onChange={(event) => setEmail(event.target.value)} />
+
+      <input 
+      type="text"
+      placeholder="Address"
+      value={address}
+      onChange={(event) => setAddress(event.target.value)} />
+
+      <input 
+      type="password"
+      placeholder="password"
+      value={password}
+      onChange={(event) => setPassword(event.target.value)} />
+
+      <p>Your name is: {name}</p>
+      <p>Your email is: {email}</p> 
+      <p>Your address is: {address}</p>
+      <p>Your password has been entered.</p>
+
+      <button type="button" onClick={handleSubmit}>
+        Create account
+      </button>
+    </div>
+
+  );
 }
