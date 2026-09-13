2fb8a687378c973c55b753d82dfdb7af2263bc14
Onyedika Christopher <cosimon.tech@gmail.com>
2026-09-13 18:11:37 +0100

    feat: connect the form to a submit handler

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -5,9 +5,37 @@ export default function Signin() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
+    const handleSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
+         console.log(email, password);
+    };
+
     return (
         <div>
             <h1>Sign in</h1>
+
+            <form onSubmit={handleSubmit}>
+
+            <input 
+            type="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            placeholder="Email"
+            />
+
+            <input 
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            placeholder="Password"
+            />
+
+            <button type="submit">
+                Sign in
+            </button>
+
+            </form>
+            
         </div>
     );
 }
